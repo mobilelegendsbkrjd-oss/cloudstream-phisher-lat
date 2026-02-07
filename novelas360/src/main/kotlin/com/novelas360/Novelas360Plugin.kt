e4d1041efeb91d2bf7ba3a43b50b4258f02a02ab
mobilelegendsbkrjd-oss <mobilelegendsbkrjd@gmail.com>
2026-02-07 13:31:59 -0600

    Update Novelas360Plugin.kt

--- novelas360/src/main/kotlin/com/novelas360/Novelas360Plugin.kt
+++ novelas360/src/main/kotlin/com/novelas360/Novelas360Plugin.kt
@@ -5,9 +5,8 @@ import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
-class Novelas360Plugin: Plugin() {
+class Novelas360Plugin : Plugin() {
     override fun load(context: Context) {
-        // Registramos la extensión con su clase principal
         registerMainAPI(Novelas360())
     }
 }
