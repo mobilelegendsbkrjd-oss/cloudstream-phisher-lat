11630865d5b65f5a485dc8047e333e902c88471b
mobilelegendsbkrjd-oss <mobilelegendsbkrjd@gmail.com>
2026-02-07 13:55:08 -0600

    Update build.gradle.kts

--- novelas360/build.gradle.kts
+++ novelas360/build.gradle.kts
@@ -1,39 +1,13 @@
-plugins {
-    id("com.android.library")
-    id("kotlin-android")
-    id("kotlin-kapt")
-}
-
-android {
-    compileSdk = 33
-
-    defaultConfig {
-        minSdk = 21
-        targetSdk = 33
-    }
-
-    sourceSets {
-        getByName("main").java.srcDirs("src/main/kotlin")
-    }
-}
-
-dependencies {
-    implementation(project(":CloudStream3")) // proyecto local
-    implementation("org.jsoup:jsoup:1.15.3")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.10")
-}
-
+// use an integer for version numbers
 version = 2
 
 cloudstream {
     description = "(Mexican) Novelas Extension"
     language = "mx"
-    authors = listOf("bkrjd")
+    authors = listOf("Bkrjd")
 
-    status = 1 // 0: Down, 1: Ok, 2: Slow, 3: Beta only
-
+    status = 1
     tvTypes = listOf("TvSeries")
     iconUrl = "https://novelas360.com/logo.png"
-
     isCrossPlatform = false
 }
