ff030eadab1eac2ec74c7eb19b38572b03d0d869
mobilelegendsbkrjd-oss <mobilelegendsbkrjd@gmail.com>
2026-02-08 22:36:48 -0600

    Update and rename SoloLatinoProviderPlugin.kt to SoloLatinoPlugin.kt

--- SoloLatino/src/main/kotlin/com/sololatino/SoloLatinoPlugin.kt
+++ SoloLatino/src/main/kotlin/com/sololatino/SoloLatinoPlugin.kt
@@ -1,13 +1,13 @@
-package com.stormunblessed
+package com.sololatino
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
-class SoloLatinoProviderPlugin: Plugin() {
+class SoloLatinoPlugin: Plugin() {
     override fun load(context: Context) {
         // All providers should be added in this manner. Please don't edit the providers list directly.
-        registerMainAPI(SoloLatinoProvider())
+        registerMainAPI(SoloLatino())
     }
 }
