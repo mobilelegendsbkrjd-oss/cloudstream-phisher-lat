56f48e28bf13f14d34ae2d9147218cc4b7e89320
mobilelegendsbkrjd-oss <mobilelegendsbkrjd@gmail.com>
2026-02-17 23:41:09 -0600

    Update CablevisionHdPlugin.kt

--- Cablevision/src/main/kotlin/com/cablevision/CablevisionHdPlugin.kt
+++ Cablevision/src/main/kotlin/com/cablevision/CablevisionHdPlugin.kt
@@ -4,6 +4,6 @@ import com.lagradost.cloudstream3.plugins.*
 
 class CablevisionHdPlugin : Plugin() {
     override fun load() {
-        registerMainAPI(CablevisionHd())
+        registerMainAPI(CablevisionHdProvider())
     }
 }
