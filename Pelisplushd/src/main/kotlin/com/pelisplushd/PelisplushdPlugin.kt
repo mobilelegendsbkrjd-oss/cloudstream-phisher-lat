15de03af8f6002a65f23edfd91ee78d8046cf29b
mobilelegendsbkrjd-oss <mobilelegendsbkrjd@gmail.com>
2026-02-18 00:37:31 -0600

    Create PelisplushdPlugin.kt

---
+++ Pelisplushd/src/main/kotlin/com/pelisplushd/PelisplushdPlugin.kt
@@ -0,0 +1,11 @@
+package com.pelisplushd
+
+import com.lagradost.cloudstream3.plugins.BasePlugin
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+
+@CloudstreamPlugin
+class PelisplushdPlugin : BasePlugin() {
+    override fun load() {
+        registerMainAPI(Pelisplushd())
+    }
+}
