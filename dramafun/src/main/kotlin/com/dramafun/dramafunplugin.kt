17e366c92d8c7f09ea57acb8143316c4a8ba4164
mobilelegendsbkrjd-oss <mobilelegendsbkrjd@gmail.com>
2026-03-10 20:03:34 -0600

    Update dramafunplugin.kt

---
+++ dramafun/src/main/kotlin/com/dramafun/dramafunplugin.kt
@@ -0,0 +1,11 @@
+package com.dramafun
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class DramaFunPlugin : Plugin() {
+    override fun load() {
+        registerMainAPI(DramaFun())
+    }
+}
