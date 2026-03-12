8e16763b5ca1e6a5f4febfb496dd733609a200c1
mobilelegendsbkrjd-oss <mobilelegendsbkrjd@gmail.com>
2026-03-11 19:21:49 -0600

    Update cuevana.kt

--- cuevana/src/main/kotlin/com/cuevana/cuevana.kt
+++ cuevana/src/main/kotlin/com/cuevana/cuevana.kt
@@ -90,7 +90,7 @@ data class VideoInfo(
 // MAIN API
 // =============================
 class Cuevana : MainAPI() {
-    override var mainUrl = "https://wv3.cuevana3.eu"
+    override var mainUrl = "https://cuevana3.re/"
     override var name = "Cuevana"
     override var lang = "es"
     override val hasMainPage = true
@@ -425,4 +425,5 @@ class Cuevana : MainAPI() {
         return found
     }
 }
+
 
